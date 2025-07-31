e57bb616524c086b45050815ffa36f2533565354
Sonu Alam <sonualam@Sonus-MacBook-Air.local>
2025-07-31 17:05:35 +0545

    feat: Implement secure OTA update system

---
+++ esp32/config.h
@@ -0,0 +1,19 @@
+// This is your configuration file.
+// It contains all secrets and should NOT be uploaded to GitHub.
+// Make sure this file is listed in your .gitignore file!
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// --- WiFi Credentials ---
+const char* ssid = "AdvancedCollege";
+const char* password = "acem@123";
+
+// --- GitHub OTA Update Credentials ---
+const char* github_user = "safal-sapkota33";
+const char* github_repo = "safal-hatchery";
+
+// PASTE YOUR NEW PERSONAL ACCESS TOKEN HERE
+const char* github_token = "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"; // Replace with your real token
+
+#endif // CONFIG_H
